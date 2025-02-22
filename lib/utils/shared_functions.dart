1eeab185a491f8373b31768d8ff4a1d878a6423e
Keronei <keronei95@gmail.com>
2025-02-22 13:01:45 +0300

    Add helper to infer error message from exception.

--- lib/utils/shared_functions.dart
+++ lib/utils/shared_functions.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 void showSnackBar(BuildContext context, String message) {
@@ -8,4 +11,18 @@ void showSnackBar(BuildContext context, String message) {
       behavior: SnackBarBehavior.floating, // Optional: Makes it float
     ),
   );
+}
+
+String getErrorMessage(Exception error) {
+  if (error is TimeoutException) {
+    return "The request timed out. Please try again.";
+  } else if (error is HttpException) {
+    return "Server error. Please try again later.";
+  } else if (error is FormatException) {
+    return "Unexpected response format. Please contact support.";
+  } else if (error is SocketException) {
+    return "No internet connection. Please check your network.";
+  } else {
+    return "Something went wrong. Please try again.";
+  }
 }
