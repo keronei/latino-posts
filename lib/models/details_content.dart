7d894903bef10eaa32410ac6d88eefe41852f07e
Keronei <keronei95@gmail.com>
2025-02-24 09:22:35 +0300

    Add flag to determine where navigation is triggered from

--- lib/models/details_content.dart
+++ lib/models/details_content.dart
@@ -3,6 +3,7 @@ import 'package:latin_news/models/news_post.dart';
 class DetailsContent {
   int selectedPostId;
   List<NewsPost> allOtherPosts;
+  bool fromBottom;
 
-  DetailsContent(this.allOtherPosts, this.selectedPostId);
+  DetailsContent(this.allOtherPosts, this.selectedPostId, this.fromBottom);
 }
